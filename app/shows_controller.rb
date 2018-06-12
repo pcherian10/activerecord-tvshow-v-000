c04df761bbbc1275f25c110deb1be81230e85cf6
Paul <pcherian10@gmail.com>
2018-06-12 23:08:37 +0000

    automatically backed up by learn

---
+++ app/shows_controller.rb
@@ -0,0 +1,28 @@
+def highest_rating
+
+end
+
+def most_popular_show
+
+end
+
+def lowest_rating
+
+end
+
+def least_popular_show
+
+end
+
+def ratings_sum
+
+end
+
+def popular_shows
+
+end
+
+def shows_by_alphabetical_order
+
+end
+
