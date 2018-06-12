cb316fca05b115d219f938f460a6f4af6a72bd3e
Paul <pcherian10@gmail.com>
2018-06-12 23:10:02 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,32 @@
 class Show < ActiveRecord::Base
+
+  def highest_rating
+    Show.maximum
+  end
+
+  def most_popular_show
+
+  end
+
+  def lowest_rating
+
+  end
+
+  def least_popular_show
+
+  end
+
+  def ratings_sum
+
+  end
+
+  def popular_shows
+
+  end
+
+  def shows_by_alphabetical_order
+
+  end
+
+  
 end
